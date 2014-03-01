103b6c9415b8f90acd28e627982a483dfc2f86a0
Shawn Leslie <shawntleslie@gmail.com>
2014-02-28 16:17:36 -0800

    Added script for typebot usecase interaction and finished 90% of styling for all sizes. Need only hovers for the other apps.

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,64 @@
 //= require_tree .
+
+(function(window, $, undefined){
+
+  // Use Case typing effect
+  $(function() {
+
+    var useCases =[
+      "social shares with more context",
+      "to drive traffic to a particular part of the page",
+      "a more elegant way to share content",
+      "to encourage which content to share",
+      "social shares with more context",
+      "to drive traffic to a particular part of the page",
+      "a more elegant way to share content",
+      "to encourage which content to share"
+    ]
+
+    var i = 0; // Sentence
+    var c = 0; // Character
+
+    var $addCharacter = $('#add-character');
+    var $useCaseList = $('#use-case-list');
+
+    function useCaseBot() {
+
+      var sentence = useCases[i%useCases.length];
+
+      c++;
+      $addCharacter.text(sentence.substr( 0, c ));
+
+      setTimeout( function() {
+        if( c <= sentence.length ) {
+          useCaseBot();
+        } else {
+          c = 0;
+          i++;
+
+          setTimeout( function(){
+            var $li = $("<li><i class='icon-ok'></i> " + sentence + "</li>").prependTo($useCaseList);
+
+            if( i > useCases.length) {
+              $useCaseList.find('li:last').remove();
+            }
+
+            setTimeout( function() {
+              $li.addClass('visible');
+            }, 0)
+
+
+            useCaseBot();
+
+          }, 1750)
+        }
+
+      }, 60);
+
+    }
+
+    useCaseBot();
+
+  });
+
+})(window, jQuery, null)
